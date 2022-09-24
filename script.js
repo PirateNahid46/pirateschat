d9a07d5ced621340ee62d20713c2b18d245c060c
Nahid Hasan <44243078+purebeast46@users.noreply.github.com>
2022-09-24 23:08:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -55,11 +55,11 @@ function fetchChatF(){
     const messages = snapshot.val();
     const time = Date(messages.id);
     if(messages.usr === username){
-      const msg = "<div class=\"my\">$" + messages.usr + " : " + messages.msg +"<br><div class=\"time\">"+times+"</div></div>";
+      const msg = "<div class=\"my\">$" + messages.usr + " : " + messages.msg +"<br><div class=\"time\">"+time+"</div></div>";
       document.getElementById("messages").innerHTML += msg;
     }
     else{
-      const msg = "<div>$" + messages.usr + " : " + messages.msg + "</div>";
+      const msg = "<div>$" + messages.usr + " : " + messages.msg +"<br><div class=\"time\">"+time+"</div></div>";
       document.getElementById("messages").innerHTML += msg;
     }
     location.href="#bottom";
