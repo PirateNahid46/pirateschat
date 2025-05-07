4ef9e03a523e03044a0f49d9dba7fb00a5a0a20d
PirateNahid <nhdnahid2022@gmail.com>
2025-05-07 09:34:31 +0600

    firebase

--- ui/script.js
+++ ui/script.js
@@ -1,12 +1,5 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAFJTHbawTSD1vw2IT50Eat5lkHuJxQlW0",
-  authDomain: "pirates-chat-d8e00.firebaseapp.com",
-  projectId: "pirates-chat-d8e00",
-  storageBucket: "pirates-chat-d8e00.appspot.com",
-  messagingSenderId: "623862060797",
-  appId: "1:623862060797:web:19809d01227cf0bf1f5094",
-  measurementId: "G-9ZDRSV4RB4"
-  };
+const timestamp = Date.now();
+console.log(timestamp)
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
   var oname;
